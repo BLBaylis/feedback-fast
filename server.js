42e82e18d24baf68dd9779fffbd77323137245f1
BLBaylis <bradleybaylis@hotmail.com>
2019-06-05 20:59:48 +0100

    Refactor server.js Passport.js configuration has been moved to the services directory and the authentication routes have been added to the routes directory.

--- server.js
+++ server.js
@@ -1,29 +1,11 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport.js");
+const authRoutes = require("./routes/auth");
 
 const app = express();
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    accessToken => console.log(accessToken)
-  )
-);
 
 app.get("/", (req, res) => res.send("Howdy!"));
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-app.get("/auth/google/callback", passport.authenticate("google"));
-
+authRoutes(app);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Hi from port ${PORT}`));
