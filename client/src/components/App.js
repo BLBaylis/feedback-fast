90621d3b7ad4d6c3e89996f0930e99259318f689
BLBaylis <bradleybaylis@hotmail.com>
2019-07-28 21:54:11 +0100

    Add redux and react router, set up eslint and prettier, App.js, index.js

---
+++ client/src/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <a href="/auth/google">LINK</a>;
+
+export default App;
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const keys = require("./config/keys");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-require("./models/Users");
-require("./services/passport");
-const authRoutes = require("./routes/auth");
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/Users');
+require('./services/passport');
+const authRoutes = require('./routes/auth');
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 const app = express();
-app.use(cookieSession({
+app.use(
+  cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-}))
+    keys: [keys.cookieKey],
+  }),
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/", (req, res) => res.send("Howdy!"));
+app.get('/', (req, res) => res.send('Howdy!'));
 authRoutes(app);
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Hi from port ${PORT}`));
+app.listen(PORT);
