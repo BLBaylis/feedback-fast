d614fbdd4bbc0ed84ec49c72faccff08a3bdde9f
BLBaylis <bradleybaylis@hotmail.com>
2019-07-29 11:24:11 +0100

    Set up redux and react router boilerplate wiht some dummy components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
 
-const App = () => <a href="/auth/google">LINK</a>;
+const dummyComp1 = () => <h1>Hi from 1</h1>;
+const dummyComp2 = () => <h1>Hi from 2</h1>;
+const dummyComp3 = () => <h1>Hi from 3</h1>;
+const dummyComp4 = () => <h1>Hi from 4</h1>;
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/1" exact component={dummyComp1} />
+          <Route path="/2" exact component={dummyComp2} />
+          <Route path="/3" exact component={dummyComp3} />
+          <Route path="/4" exact component={dummyComp4} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
